a9014ffe96fcbc618fae1af4f33376c782fb892a
fan <wangyufan1004@gmail.com.cn>
2018-11-13 01:43:53 +0800

    fixed some warning

--- lessonTwo/chinese_seq2seq_chatbot/getConfig.py
+++ lessonTwo/chinese_seq2seq_chatbot/getConfig.py
@@ -3,7 +3,7 @@ import os
 import random
 import sys
 import time
-
+from importlib import reload
 import numpy as np
 
 
@@ -16,16 +16,18 @@ except NameError:
 else:
     reload(sys).setdefaultencoding('utf-8')
     
-try:
-    from ConfigParser import SafeConfigParser
-except:
-    from configparser import SafeConfigParser # In Python 3, ConfigParser has been renamed to configparser for PEP 8 compliance.
+# from configparser import SafeConfigParser
+
+from configparser import ConfigParser
+
+# In Python 3, ConfigParser has been renamed to configparser for PEP 8 compliance.
+
 
 def get_config(config_file='seq2seq.ini'):
-    parser = SafeConfigParser()
+    parser = ConfigParser()
     parser.read(config_file)
     # get the ints, floats and strings
-    _conf_ints = [ (key, int(value)) for key,value in parser.items('ints') ]
-    _conf_floats = [ (key, float(value)) for key,value in parser.items('floats') ]
-    _conf_strings = [ (key, str(value)) for key,value in parser.items('strings') ]
+    _conf_ints = [(key, int(value)) for key, value in parser.items('ints')]
+    _conf_floats = [(key, float(value)) for key, value in parser.items('floats')]
+    _conf_strings = [(key, str(value)) for key, value in parser.items('strings')]
     return dict(_conf_ints + _conf_floats + _conf_strings)
